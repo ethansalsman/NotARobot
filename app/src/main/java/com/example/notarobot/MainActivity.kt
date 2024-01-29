cbe6daf2698aedf5061bc503e6fd0205f5491cac
ethansalsman <122046127+ethansalsman@users.noreply.github.com>
2024-01-29 15:33:13 -0400

    progress

--- app/src/main/java/com/example/notarobot/MainActivity.kt
+++ app/src/main/java/com/example/notarobot/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.notarobot.ui.theme.NotARobotTheme
@@ -17,25 +19,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotARobotTheme {
-                // A surface container using the 'background' color from the theme
+                // A surface container using the 'background' color from the them
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
-
+data class Animal(val imageRes: Int, val isCat: Boolean)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NotARobot() {
+    val animals = remember { generateAnimalList }
+    var feedbackMessage by remember { mutableStateOf<String?>(null) }
+
 
 @Preview(showBackground = true)
 @Composable
